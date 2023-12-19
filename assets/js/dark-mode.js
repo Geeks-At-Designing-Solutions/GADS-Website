9bd71d03c02eee8155df6c0fb2840c726f8d4c9f
Abhishek <abhishekvm819@gmail.com>
2023-12-19 23:23:34 +0530

    Christmas update

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,17 +1,34 @@
 let button = document.querySelector('.dark-mode-btn')
+
 //dark mode by gads
 button.addEventListener('click', () => {
+
 document.documentElement.classList.toggle('dark-mode')
+
 if (document.getElementById("moon").classList.contains('fa-moon')) {
+
 document.getElementById("moon").classList.add('fa-sun');
 document.getElementById("moon").classList.remove('fa-moon');
 }else{
 document.getElementById("moon").classList.add('fa-moon');
 document.getElementById("moon").classList.remove('fa-sun');
 }
+
 document.querySelectorAll('.inverted').forEach((result)=>{
   result.classList.toggle('invert')
 })
+
+let element = document.querySelector('.snowfall');
+
+let currentFilter = element.style.filter;
+
+let newFilter = currentFilter === 'brightness(0)' ? 'brightness(100)' : 'brightness(0)';
+
+element.style.filter = newFilter;
+
+document.getElementById("hero").classList.toggle('inv');
+console.log('changed');
+
 })
 
 let mobileBtn = document.querySelector('.dark-mode-on-mobile')
@@ -28,6 +45,17 @@ mobileBtn.addEventListener('click', () => {
   document.querySelectorAll('.inverted').forEach((result)=>{
     result.classList.toggle('invert')
   })
+
+  let element = document.querySelector('.snowfall');
+
+let currentFilter = element.style.filter;
+
+let newFilter = currentFilter === 'brightness(0)' ? 'brightness(100)' : 'brightness(0)';
+
+element.style.filter = newFilter;
+
+document.getElementById("hero").classList.toggle('inv');
+console.log('changed');
   })
   
   $(function () {
